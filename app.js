1e2b12d7cf651b62d7c273dcff7634335534ddd9
Gedo <43795422+abualhassan30@users.noreply.github.com>
2022-09-05 02:08:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// document.querySelector("h1").style.color = "pink";
+import translations from "/translations.js";
+
+const languageSelector = document.querySelector("select");
+languageSelector.addEventListener("change", (event) => {
+  setLanguage(event.target.value);
+  localStorage.setItem("lang", event.target.value);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const language = localStorage.getItem("lang") || "ar"; // اذا لم تكن اللغة متوفرة استخدم الانجليزية
+  setLanguage(language);
+});
+
+const setLanguage = (language) => {
+  const elements = document.querySelectorAll("[data-i18n]");
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute("data-i18n");
+    element.textContent = translations[language][translationKey];
+  });
+  document.dir = language === "ar" ? "rtl" : "ltr";
+};
+
+
+
+// FOOTER & TIME AND DATE
+  const options = {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+  };
+
+  var dt = new Date();
+  document.getElementById("datetime").innerHTML = dt.toLocaleString();
---
+++ translations.js
@@ -0,0 +1,222 @@
+const translations = {
+  en: {
+    ara: "arabic",
+    eng: "English",
+    myname: "ABUALHASSAN ABDUALLATIF",
+    myjob: "Financial Accountant & Business Developer",
+    intro:
+      "I am interested in data analysis & problems solving. Business development and innovations are my hobby and passion. I find pleasure in deep thinking and thinking outside the box.",
+    service1: "Financial Accounting",
+    service2: "Financial analysis",
+    service3: "financial planning",
+    service4: "financial advisory",
+    service5: "Business Development",
+
+    firstjob: "Bannotti Trading Est",
+    firstwork_job: "Financial Accountant & Executive Assistant",
+    firstwork_job_period: "2016 to 2018",
+    firstwork_job_specialist: "A company Specializing in women's accessories",
+
+    secondjob: "Althega Aldaemah Office",
+    secondwork_job: "Financial accountant & follow up",
+    secondwork_job_period: "2018 to 2019",
+    secondwork_job_specialist: "Office for the recruitment of domestic workers",
+
+    thirdjob: "Ibrahim Saeed Alsubiat Est",
+    thirdwork_job: "Financial Accountant & Administrative Assistant",
+    thirdwork_job_period: "May 2019 to May 2022",
+    thirdwork_job_specialist:
+      "A company specializing in lighting and accessories",
+
+    // ACCOUNTAT LIST TASKS
+    acc_tasks: "Accountant Tasks",
+    acc_list_1: "Accounting cycle - starting from the journal entry",
+    acc_list_2: "Enter revenues, purchases and expenses.",
+    acc_list_3: "Follow up and match customer and supplier accounts.",
+    acc_list_4: "Preparing salary marches and extracting monthly percentages",
+    acc_list_5: "Matching accounts of funds and delegates.",
+    acc_list_6:
+      "Matching and reviewing the covenant of the facility's employees.",
+    acc_list_7: "Reconciling and reconciling bank accounts.",
+    acc_list_8: "Inventory and matching with the system.",
+    acc_list_9: "Sales and Purchasing Analysis",
+    acc_list_10: "Raise tax returns",
+    acc_list_11:
+      "Preparing the necessary financial statements and periodic reports.",
+    acc_list_12: "Develop financial plans to support decision-making",
+    acc_list_13:
+      "Planning to avoid financial crises and manage financial risks.",
+    acc_list_14: "All other work related to accounting.",
+    // MANAGEMENTS LIST TASKS
+    manager_taks: "Administrative and executive tasks",
+    manager_taks1: "Managing exhibitions inside and outside Riyadh.",
+    manager_taks2: "Develop and implement expansion plans.",
+    manager_taks3: "Collect problems and find appropriate solutions.",
+    manager_taks4: "Assisting in setting annual, monthly and seasonal goals.",
+    manager_taks5:
+      "Continuous development and innovation to improve the level of the institution.",
+    manager_taks6: "HR management",
+    manager_taks7: "Follow-up agency externals from several countries.",
+    manager_taks8:
+      "Termination of financial transactions and belongings with external agents",
+    manager_taks9: "Translating notes and problems of workers",
+    manager_taks10: "Dealing with electronic platforms",
+    manager_taks11: "Providing management consulting",
+    manager_taks12: "Planning and development",
+    manager_taks13: "Organization and administrative control",
+
+    // -------------Qualification Section-----------
+    qualif_1: "Qualifications",
+    degree_1: "Accountatnt information system",
+    year_1: "El-Neelain University - 2011 - 2014",
+
+    degree_2: "English language diploma",
+    year_2: "american discussion club | 2014 - 2015",
+
+    degree_3: "Diploma in Financial Management",
+    year_3: "IBMI institute - 2020",
+    auth_link1: "Verification of Certificates",
+    Cert_Code1: "Certificate Code | 553712-165-671-0905",
+
+    degree_4: "Fundamentals of digital marketing",
+    year_4: "Google - 2021",
+    auth_link2: "Verification of Certificates",
+    Cert_Code2: "Certificate Code | 3CV ND3 DWY",
+
+    degree_5: "E-accounting diploma",
+    year_5: "Sudatel Telecom Group (STG) 2015",
+
+    degree_6: "PMP Certification Exam Preparation",
+    year_6: "Multiple Resources",
+
+    // --------------Skils + Hopes + Data-------------
+
+    skils: "skils",
+    plan_found: "Planning and Foundation",
+    imple_org: "Implementation and organization",
+    super_contr: "Supervision and control",
+    acc_soft: "accounting software skils",
+    depart_manag: "Department and team management",
+    compu_Driv: "Computer & Drivers",
+
+    interests: "Interests",
+    reading: "Reading",
+    tech: "Technology",
+    self_taught: "Self-taught",
+
+    informations: "data",
+    nationality: "Nationality: Sudan",
+    occ: "Occupation: General accountant",
+    email: "Email: Gedottt@gmail.com",
+
+    // ---------FOTTER--------
+    fotter: "",
+  },
+
+  ar: {
+    ara: "العربية",
+    eng: "الانجليزية",
+    myname: "أبوالحسن عبداللطيف",
+    myjob: "محاسب مالـي & مطور أعمال",
+    intro:
+      "مهتم بتحليل البيانات وايجاد حلول للمشاكل المتعثرة، كما أن تطوير الاعمال والابتكارات هوايتي وشقفي، اجد لذة في التفكير العميق والتفكير خارج الصندوق.",
+    service1: "المحاسبة المالية",
+    service2: "التحليل المالي",
+    service3: "التخطيط المالي",
+    service4: "الاستشارات المالية",
+    service5: "تطوير الأعمال",
+
+    firstjob: " مؤسسة بنوتتي للتجارة",
+    firstwork_job: "محاسب مالي & مساعد تنفيذي",
+    firstwork_job_period: "2016 الـي 2018",
+    firstwork_job_specialist: "متخصصة في الاكسسوارات النسائية",
+
+    secondjob: "مكتب الثقة الدائمة للاستقدام",
+    secondwork_job: "محاسب مالي & متابعة",
+    secondwork_job_period: "ابريل 2018 الــي 2019",
+    secondwork_job_specialist: "مكتب لاستقدام العمالة المنزلية",
+
+    thirdjob: "مؤسسة إبراهيم سعيد السبيت التجارية",
+    thirdwork_job: "محاسب مالي & مساعد إداري",
+    thirdwork_job_period: "مايو 2019 الــي مايو 2022",
+    thirdwork_job_specialist: "مؤسسة متخصصة في الانارة ومستلزماتها",
+
+    // ترجمة قائمة مهام المحاسبة
+    acc_tasks: "الهـام المحاسـبية",
+    acc_list_1: "الدورة المحاسبية - ابتداء من قيد اليومية",
+    acc_list_2: "ادخال الايرادات والمشتريات والمصروفات.",
+    acc_list_3: "متابعة حسابات العملاء والموردين ومطابقتها.",
+    acc_list_4: "اعداد مسيرات الرواتب و استخراج النسب الشهرية",
+    acc_list_5: "مطابقة حسابات الصناديق والمناديب.",
+    acc_list_6: "مطابقة ومراجعة عهد العاملين بالمنشاة.",
+    acc_list_7: "مطابقة الحسابات البنكية وتسويتها.",
+    acc_list_8: "جرد المخزون ومطابقته مع النظام.",
+    acc_list_9: "تحليل المبيعات والمشتريات",
+    acc_list_10: "رفع الاقرارات الضريبة",
+    acc_list_11: "اعداد القوائم المالية والتقارير الدورية الالزمة.",
+    acc_list_12: "ضع الخطط الماليه لدعم اتخاذ القرارات المساعدة",
+    acc_list_13: "التخطيط لتجنب الازمات المالية وادارة المخاطر المالية.",
+    acc_list_14: "جميع الاعمال الاخرى المتعلقة بالمحاسبة.",
+
+    // ترجمة الهام الادارية والتنفيذية
+    manager_taks: " المهـــام الادارية والتنفيذية",
+    manager_taks1: "ادارة المعارض داخل وخارج الرياض.",
+    manager_taks2: "وضع الخطط التوسعية وتنفيذها.",
+    manager_taks3: "جمع المشاكل و ايجاد الحلول المناسبة لها.",
+    manager_taks4: "المساعدة علي وضع الاهداف السنوية والشهرية والموسمية.",
+    manager_taks5: "التطوير والابتكار المستمر لتحسين مستوي الموسسة.",
+    manager_taks6: "إدارة الموارد البشرية HR",
+    manager_taks7: "متابعة الوكالة الخارجيين من عدة دول.",
+    manager_taks8: "إنهاء التعاملات و المتعلقات المالية مع الوكلاء الخارجيين",
+    manager_taks9: "ترجمة ملاحظات ومشاكل العاملات",
+    manager_taks10: "التعامل علي المنصات الالكترونية",
+    manager_taks11: "تقديم الاستشارات الادارية",
+    manager_taks12: "التخطيط و التطوير",
+    manager_taks13: "التنظيم والضبط الاداري",
+
+    // -----------المؤهلات العلمــــــية-----------
+    qualif_1: "المؤهلات",
+    degree_1: "نظم معلومات محاسبية",
+    year_1: "جامعة النيلين | 2011 - 2014",
+
+    degree_2: "دبلوم لغة انجليزية",
+    year_2: "النادي الامريكي للمخطابة | 2014 - 2015",
+
+    degree_3: "دبلوم الادارة المالـية",
+    year_3: "معهد IBMI",
+    auth_link1: "رابط التحقق من الشهادة اضغط هنا",
+    Cert_Code1: "معرف بيان الاعتماد | 553712-165-671-0905",
+
+    degree_4: "اساسيات التسويق الرقمي",
+    year_4: "قوقل | 2021م",
+    auth_link2: "رابط التحقق من الشهادة اضغط هنا",
+    Cert_Code2: "معرف بيان الاعتماد | 3CV ND3 DWY",
+
+    degree_5: "دبلوم المحاسبة الالكترونية",
+    year_5: "اكاديمية سوداكاد (سوداتل) | 2015م",
+
+    degree_6: "التحضير لاختبار PMP",
+    year_6: "النادي الامريكي & العديد من المصادر",
+
+    // ---------المهارات + الهوايات + البيانات----------
+    skils: "المهـارات",
+    plan_found: "التخطيط و التأسيس",
+    imple_org: "التنفيذ والتنظيم",
+    super_contr: "الاشراف والرقابة",
+    acc_soft: "البرامج المحاسبية",
+    depart_manag: "إدارة الاقسام والفرق",
+    compu_Driv: "الكمبيوتر & برامجه التشغيلية",
+
+    interests: "الهوايات",
+    reading: "القراءة",
+    tech: "التقنية",
+    self_taught: "التعلم الذاتي",
+
+    informations: "البيانات",
+    Nationality: "الجنسية: السـودان",
+    occ: "المهنة: محاسب عام",
+    email: "الايميل: Gedottt@gmail.com",
+  },
+};
+
+export default translations;
